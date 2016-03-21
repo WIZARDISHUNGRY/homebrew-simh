256b64a63b6c2f2bcca29e512f7dc321fce42843
Jon Williams <jonathan.williams@gmail.com>
2016-03-21 18:21:01 -0400

    fixes simh/simh#292 ; reimplements simh/simh#293

--- Formula/simh.rb
+++ Formula/simh.rb
@@ -6,11 +6,14 @@ class Simh < Formula
 
   head "https://github.com/simh/simh.git"
 
+  depends_on "sdl2"
+
   def install
     ENV.deparallelize unless build.head?
     inreplace "makefile", "GCC = gcc", "GCC = #{ENV.cc}"
     inreplace "makefile", "CFLAGS_O = -O2", "CFLAGS_O = #{ENV.cflags}"
-    system "make",  "USE_NETWORK=1", "INCPATH=/usr/local/include", "LIBPATH=/usr/local/lib", "all"
+    inreplace "makefile", "/opt/local", "/usr/local"
+    system "make", "all"
     bin.install Dir["BIN/*"]
     Dir["**/*.txt"].each do |f|
       (doc/File.dirname(f)).install f
